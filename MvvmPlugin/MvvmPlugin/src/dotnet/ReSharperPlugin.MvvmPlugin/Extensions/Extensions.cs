2bab20ab7f49df1625324d7f47463b73d00c02b4
Jeppe Roi Kristensen <jeppe@jeppek.dk>
2025-01-25 00:32:31 +0100

    Add "Convert to Observable Property" context action

--- MvvmPlugin/MvvmPlugin/src/dotnet/ReSharperPlugin.MvvmPlugin/Extensions/Extensions.cs
+++ MvvmPlugin/MvvmPlugin/src/dotnet/ReSharperPlugin.MvvmPlugin/Extensions/Extensions.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using JetBrains.ProjectModel.Properties.CSharp;
+using JetBrains.ReSharper.Psi;
 
 namespace ReSharperPlugin.MvvmPlugin.Extensions;
 
@@ -17,4 +18,29 @@ public static class Extensions
         
         return defaultValue;
     }
+
+    // public static bool InheritsFrom(this ITypeElement type, IDeclaredType baseType, HashSet<string>? testedTypes = null)
+    // {
+    //     testedTypes ??= new HashSet<string>();
+    //     var baseTypes = type.GetSuperTypeElements();
+    //
+    //     while (baseTypes is {Count: > 0})
+    //     {
+    //         foreach (var baseTypeElement in baseTypes)
+    //         {
+    //             // if the type has already been tested we continue (just to ensure
+    //             // that we don't create a stack overflow
+    //             if (!testedTypes.Add(baseTypeElement.GetClrName().FullName))
+    //                 continue;
+    //
+    //             if (baseTypeElement.IsValid() && baseTypeElement.Equals(baseType))
+    //                 return true;
+    //
+    //             if (InheritsFrom(baseTypeElement, baseType, testedTypes))
+    //                 return true;
+    //         }
+    //     }
+    //
+    //     return false;
+    // }
 }
