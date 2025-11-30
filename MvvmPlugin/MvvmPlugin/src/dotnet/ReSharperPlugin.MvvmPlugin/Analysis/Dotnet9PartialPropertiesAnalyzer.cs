9b7f7e479d74ac3424dcd46e3feefb9bec899a28
Jeppe Roi Kristensen <jeppe@jeppek.dk>
2025-11-30 21:15:23 +0100

    Disabled Dotnet9 warning

--- MvvmPlugin/MvvmPlugin/src/dotnet/ReSharperPlugin.MvvmPlugin/Analysis/Dotnet9PartialPropertiesAnalyzer.cs
+++ MvvmPlugin/MvvmPlugin/src/dotnet/ReSharperPlugin.MvvmPlugin/Analysis/Dotnet9PartialPropertiesAnalyzer.cs
@@ -10,20 +10,20 @@ using ReSharperPlugin.MvvmPlugin.Models;
 
 namespace ReSharperPlugin.MvvmPlugin.Analysis;
 
-[ElementProblemAnalyzer(typeof(IClassLikeDeclaration), HighlightingTypes = new[] {typeof(Dotnet9RequiredWarning)})]
-public class Dotnet9PartialPropertiesAnalyzer : ElementProblemAnalyzer<IClassLikeDeclaration>
-{
-    protected override void Run(IClassLikeDeclaration element, ElementProblemAnalyzerData data, IHighlightingConsumer consumer)
-    {
-        if (PluginUtil.GetObservableObject(element) is { } observable && element.DeclaredElement?.IsDescendantOf(observable.GetTypeElement()) == true)
-        {
-            if (element.GetCSharpProjectConfiguration() is { } configuration)
-            {
-                if (configuration is {LanguageVersion: CSharpLanguageVersion.Preview, TargetFrameworkId: { Version.Major: > 9 } target} && (target.IsNetCore || target.IsNetCoreApp) && observable.Assembly?.Version >=  new Version(8,4))
-                {
-                    consumer.AddHighlighting(new Dotnet9RequiredWarning(element, element.GetNameDocumentRange()));
-                }
-            }
-        }
-    }
-}
+// [ElementProblemAnalyzer(typeof(IClassLikeDeclaration), HighlightingTypes = new[] {typeof(Dotnet9RequiredWarning)})]
+// public class Dotnet9PartialPropertiesAnalyzer : ElementProblemAnalyzer<IClassLikeDeclaration>
+// {
+//     protected override void Run(IClassLikeDeclaration element, ElementProblemAnalyzerData data, IHighlightingConsumer consumer)
+//     {
+//         if (PluginUtil.GetObservableObject(element) is { } observable && element.DeclaredElement?.IsDescendantOf(observable.GetTypeElement()) == true)
+//         {
+//             if (element.GetCSharpProjectConfiguration() is { } configuration)
+//             {
+//                 if (configuration is {LanguageVersion: CSharpLanguageVersion.Preview, TargetFrameworkId: { Version.Major: > 9 } target} && (target.IsNetCore || target.IsNetCoreApp) && observable.Assembly?.Version >=  new Version(8,4))
+//                 {
+//                     consumer.AddHighlighting(new Dotnet9RequiredWarning(element, element.GetNameDocumentRange()));
+//                 }
+//             }
+//         }
+//     }
+// }
--- MvvmPlugin/MvvmPlugin/src/dotnet/ReSharperPlugin.MvvmPlugin/Analysis/Dotnet9RequiredWarning.cs
+++ MvvmPlugin/MvvmPlugin/src/dotnet/ReSharperPlugin.MvvmPlugin/Analysis/Dotnet9RequiredWarning.cs
@@ -4,28 +4,28 @@ using JetBrains.ReSharper.Psi.CSharp.Tree;
 
 namespace ReSharperPlugin.MvvmPlugin.Analysis;
 
-[StaticSeverityHighlighting(Severity.ERROR, typeof(HighlightingGroupIds.GutterMarks))]
-public class Dotnet9RequiredWarning : IHighlighting
-{
-    private readonly IClassLikeDeclaration myClassDeclaration;
-    private readonly DocumentRange myRange;
-
-    public Dotnet9RequiredWarning(IClassLikeDeclaration classDeclaration, DocumentRange range)
-    {
-        myClassDeclaration = classDeclaration;
-        myRange = range;
-    }
-    
-    public bool IsValid()
-    {
-        return myClassDeclaration.IsValid();
-    }
-
-    public DocumentRange CalculateRange()
-    {
-        return myRange;
-    }
-
-    public string? ToolTip => "To support partial properties you must be using .NET 9 or higher";
-    public string? ErrorStripeToolTip { get; }
-}
+// [StaticSeverityHighlighting(Severity.ERROR, typeof(HighlightingGroupIds.GutterMarks))]
+// public class Dotnet9RequiredWarning : IHighlighting
+// {
+//     private readonly IClassLikeDeclaration myClassDeclaration;
+//     private readonly DocumentRange myRange;
+//
+//     public Dotnet9RequiredWarning(IClassLikeDeclaration classDeclaration, DocumentRange range)
+//     {
+//         myClassDeclaration = classDeclaration;
+//         myRange = range;
+//     }
+//     
+//     public bool IsValid()
+//     {
+//         return myClassDeclaration.IsValid();
+//     }
+//
+//     public DocumentRange CalculateRange()
+//     {
+//         return myRange;
+//     }
+//
+//     public string? ToolTip => "To support partial properties you must be using .NET 9 or higher";
+//     public string? ErrorStripeToolTip { get; }
+// }
